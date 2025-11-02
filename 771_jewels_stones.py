512ad41aded32e70399e959d4f6bbdcb75a17d6b
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2025-11-02 13:52:12 +0530

    771 using 2 for loops

--- 771_jewels_stones.py
+++ 771_jewels_stones.py
@@ -21,11 +21,17 @@
 
 def func(jewels,stones):
     count=0
-    for i in stones:
-        if i in jewels:
-            count+=1
-        else:
-            pass
+    # for i in stones:
+    #     if i in jewels:
+    #         count+=1
+    #     else:
+    #         pass
+    for i in jewels:
+        for j in stones:
+            if i==j:
+                count+=1
+            else:
+                pass
     return count 
 result=func("aAbB","AabbCCC")
 print(result)
