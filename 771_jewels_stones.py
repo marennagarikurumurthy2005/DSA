8d5c24b7c01f75d8b5c475e01d3066ab15aafec9
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2025-11-02 13:45:15 +0530

    771 Problem

---
+++ 771_jewels_stones.py
@@ -0,0 +1,31 @@
+# 771. Jewels and Stones
+# Easy
+# Topics
+# premium lock icon
+# Companies
+# Hint
+# You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+# Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+ 
+
+# Example 1:
+
+# Input: jewels = "aA", stones = "aAAbbbb"
+# Output: 3
+# Example 2:
+
+# Input: jewels = "z", stones = "ZZ"
+# Output: 0
+
+def func(jewels,stones):
+    count=0
+    for i in stones:
+        if i in jewels:
+            count+=1
+        else:
+            pass
+    return count 
+result=func("aAbB","AabbCCC")
+print(result)
