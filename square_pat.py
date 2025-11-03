bd10e07b1cab28ea313de1179ac4956014d5eb20
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2025-11-03 18:56:44 +0530

    diamond,ben10,and few

--- square_pat.py
+++ square_pat.py
@@ -19,11 +19,46 @@
 #             print("  ",end="")
 #     print("")
 
-r=10
-c=5
-for i in range(r,0,-1):
-    for j in range(i,0,-1):
-        print(" ",end="")
-    for k in range(c):
-        print("-",end='')
+# r=20
+# c=10
+# for i in range(r):
+#     for j in range(r-i-1):
+#         print(" ",end="")
+#     for k in range(c):
+#         print("*",end='')
+#     print()
+
+r=20
+c=19
+# for i in range(r):
+#     for j in range(0,i):
+#         print(" ",end="")
+#     for k in range(c):
+#         print("/",end='')
+#     print()
+
+
+
+for i in range(r-1,-1,-1):
+    for j in range(c-i):
+        print(" ",end='')
+    temp=i*2+1
+    for k in range(temp):
+        print("*",end="")
+    
+    
     print()
+
+for i in range(r-1):
+    for j in range(c-i):
+        print(" ",end='')
+    temp=i*2+1
+    for k in range(temp):
+        print("*",end="")
+    
+    
+    print()
+
+
+
+
