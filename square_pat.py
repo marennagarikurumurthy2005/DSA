1b0d4f577bd5bbe136ab730698f2a6527cd73ab7
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2025-11-03 15:51:16 +0530

    halo square

--- square_pat.py
+++ square_pat.py
@@ -1,8 +1,22 @@
-for i in range(0,5):
-    for j in range(0,50):
-        print("*",end='')
-        if j<49:
-            print("-",end='')
+# for i in range(0,5):
+#     for j in range(0,50):
+#         print("*",end='')
+#         if j<49:
+#             print("-",end='')
+#         else:
+#             pass
+#     print("")
+
+
+
+r=10
+c=10
+for i in range(0,r):
+    for j in range(0,c):
+        if i==0 or i==r-1:
+            print("*",end='')
+        elif j==0 or j==c-1:
+            print("*",end='')
         else:
-            pass
+            print(" ",end="")
     print("")
