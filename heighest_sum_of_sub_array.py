7c973edc48cc715a5fc25302ac587e3c48449655
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2025-11-16 19:12:48 +0530

    sliding window

--- heighest_sum_of_sub_array.py
+++ heighest_sum_of_sub_array.py
@@ -1,16 +1,33 @@
 
 # brute force
 
-nums=[2,4,8,6,8]
-n=len(nums)
-maxi=0
-for i in range(n-2):
-    for j in range(i,i+3):
-        arr=[]
-        for k in range(i,j+1):
-            arr.append(nums[k])   
-        if len(arr)>=3:
-            val=sum(arr)
-            maxi=max(maxi,val)
-print(maxi)         
+# nums=[2,4,8,6,8]
+# n=len(nums)
+# maxi=0
+# for i in range(n-2):
+#     for j in range(i,i+3):
+#         sub=[]
+#         for k in range(i,j+1):
+#             sub.append(nums[k])   
+#         if len(sub)>=3:
+#             val=sum(sub)
+#             maxi=max(maxi,val)
+# print(maxi)         
 
+
+
+# sliding window O(n)
+nums=[2,4,8,6,8]
+l=0
+sublen=3
+n=len(nums)
+temp=0
+maxi=0
+for r in range(n):
+    temp+=nums[r]
+    if r-l==sublen:
+        temp-=nums[l]
+        l+=1
+    if r-l+1==sublen:
+        maxi=max(maxi,temp)
+print(maxi)
